89cea51f51e943c641f33c204fca9caa0542d1f1
Temain <temain@mail.ru>
2014-03-25 15:48:50 +0400

    Create Config module

---
+++ lib/config.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+require 'highline/import'
+
+module Config
+  
+  def self.configure
+    file_path = File.expand_path('../../config.yml', __FILE__)
+    @config   = YAML.load_file(file_path)
+  rescue Errno::ENOENT, Psych::SyntaxError # file not found, syntax error
+    puts "Config file not found or contains invalid syntax, so please enter required data."
+    api       = 'https://api.vk.com/method/'
+    client_id = ask("Enter your app id:  ", Integer) 
+    user_id   = ask("Enter your user id:  ", Integer) 
+    email     = ask("Enter your email:  ", String) { |q| q.validate = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    pass      = ask("Enter your password:  ", String) { |q| q.echo = false }
+    @config   = { "api" => api, "user" => { "client" => client_id, "id" => user_id, "email" => email, "pass" => pass } }
+    File.open(file_path, 'w') do |file|
+      file.write @config.to_yaml
+    end
+    @config
+  end
+  
+end
